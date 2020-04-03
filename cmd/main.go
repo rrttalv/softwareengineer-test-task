7ef58a763cef61905fa86c2addf4fac8785f4694
Rico Rein Talvar <ritalv@ttu.ee>
2020-04-03 21:53:10 +0300

    client server structure

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,10 @@ const (
 	addr = "localhost:8000"
 )
 
-var router *gin.Engine
+type clientServer struct {
+	router *gin.Engine
+	client service.TicketServiceClient
+}
 
 func main() {
 	conn, err := grpc.Dial(addr, grpc.WithInsecure())
@@ -28,65 +31,72 @@ func main() {
 	}
 	client := service.NewTicketServiceClient(conn)
 	router := gin.Default()
-	router.POST("/aggregated", func(ctx *gin.Context) {
-		dateRange := &service.DateRange{}
-		b, _ := ioutil.ReadAll(ctx.Request.Body)
-		err := json.Unmarshal(b, &dateRange)
-		if err != nil {
-			panic(err)
-		}
-		stream, err := client.GetAggregatedCategory(context.Background(), dateRange)
-		if err != nil {
-			panic(err)
-		}
-		var results []*service.Categories
-		for {
-			cat, err := stream.Recv()
-			if err == io.EOF {
-				log.Println("end")
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			results = append(results, cat)
-		}
-		ctx.JSON(http.StatusOK, gin.H{
-			"results": results,
-		})
-		return
-	})
-	router.POST("/byticket", func(ctx *gin.Context) {
-		dateRange := &service.DateRange{}
-		b, _ := ioutil.ReadAll(ctx.Request.Body)
-		err := json.Unmarshal(b, &dateRange)
-		if err != nil {
-			panic(err)
-		}
-		stream, err := client.GetScoresByTickets(context.Background(), dateRange)
-		if err != nil {
-			panic(err)
-		}
-		var results []*service.TicketScores
-		for {
-			tik, err := stream.Recv()
-			if err == io.EOF {
-				log.Println("end")
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-			results = append(results, tik)
-		}
-		ctx.JSON(http.StatusOK, gin.H{
-			"results": results,
-		})
-		return
-	})
-
+	cs := clientServer{
+		router: router,
+		client: client,
+	}
+	router.POST("/aggregated", cs.GetAggregated)
+	router.POST("/byticket", cs.GetScoresByTicket)
 	if err := router.Run(port); err != nil {
 		panic(err)
 	}
+}
+
+func (cs *clientServer) GetAggregated(ctx *gin.Context) {
+	dateRange := &service.DateRange{}
+	b, _ := ioutil.ReadAll(ctx.Request.Body)
+	err := json.Unmarshal(b, &dateRange)
+	if err != nil {
+		panic(err)
+	}
+	stream, err := cs.client.GetAggregatedCategory(context.Background(), dateRange)
+	if err != nil {
+		panic(err)
+	}
+	var results []*service.Categories
+	for {
+		cat, err := stream.Recv()
+		if err == io.EOF {
+			log.Println("end")
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, cat)
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"results": results,
+	})
+	return
+}
+
+func (cs *clientServer) GetScoresByTicket(ctx *gin.Context) {
+	dateRange := &service.DateRange{}
+	b, _ := ioutil.ReadAll(ctx.Request.Body)
+	err := json.Unmarshal(b, &dateRange)
+	if err != nil {
+		panic(err)
+	}
+	stream, err := cs.client.GetScoresByTickets(context.Background(), dateRange)
+	if err != nil {
+		panic(err)
+	}
+	var results []*service.TicketScores
+	for {
+		tik, err := stream.Recv()
+		if err == io.EOF {
+			log.Println("end")
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, tik)
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"results": results,
+	})
+	return
 }
